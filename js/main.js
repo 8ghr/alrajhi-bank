c771f1b3295b1b130fedfcbce7c7238300677bb5
8ghr <90587700+8ghr@users.noreply.github.com>
2022-03-01 05:02:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// let ob = { // this is how to write the object
+//     fruit: "apple",
+//     vegetables: 'orange',
+//     how_many_eat_everyday: 20,
+// };
+// console.log(ob);
+// let ob = { // this is how to add and delete key of object
+//     fruit: "apple",
+//     vegetables: 'orange',
+//     how_many_eat_everyday: 20,
+// };
+// ob.country = 'saudi arabia';
+// delete ob.fruit
+// console.log(ob)
+// let ob = { // this is how
+//     fruit: "apple",
+//     vegetables: 'orange',
+//     how_many_eat_everyday: 20,
+// };
+// // if(ob.fruit === undefined) {
+// //     console.log('i dont have value');
+// // }
+// console.log('fruit' in ob);
+// let ob = { // this is
+//     fruit: "apple",
+//     vegetables: 'orange',
+//     how_many_eat_everyday: 20,
+// };
+// for(let keys in ob) {
+//     console.log(ob[keys])
+// }
